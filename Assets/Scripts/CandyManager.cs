c1e31d325ccc0ab7d557a5f850571a325f1c9a0e
suzuki <kumanojudoka@gmail.com>
2025-04-12 11:08:11 +0900

    積み防止

--- Assets/Scripts/CandyManager.cs
+++ Assets/Scripts/CandyManager.cs
@@ -1,29 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CandyManager : MonoBehaviour
 {
     const int DefaultCandyAmount = 30;
+    const int RecoverySeconds = 10;
+    public TextMeshProUGUI candyUI;
+    int counter;
+    int candy = DefaultCandyAmount;
 
-    public int candy = DefaultCandyAmount;
+    void Start()
+    {
+        UpdateUI();
+    }
 
+    void Update()
+    {
+        if(candy < DefaultCandyAmount && counter <= 0){
+            StartCoroutine(RecoverCandy());
+        }
+    }
+
+    public int Candy{
+        get{return this.candy;}
+    }
+    
     public void ConsumeCandy(){
         if(candy > 0) candy--;
+        UpdateUI();
     }
 
-    public int GetCandyAmount(){
-        return candy;
+    void UpdateUI(){
+        string cnt = "";
+        if(counter > 0){
+            cnt = $"({counter}s)";
+        }
+        candyUI.text = $"Candy : {candy}{cnt}";
     }
+
+    // public int GetCandyAmount(){
+    //     return candy;
+    // }
 
     public void AddCandy(int amount){
         candy += amount;
+        UpdateUI();
     }
 
-    void OnGUI()
-    {
-        GUI.color = Color.black;
-        string label = "Candy :" + candy;
-        GUI.Label(new Rect(50,50,100,30),label);
+    // void OnGUI()
+    // {
+    //     GUI.color = Color.black;
+    //     string label = "Candy :" + candy;
+    //     GUI.Label(new Rect(50,50,100,30),label);
+    //     if(counter > 0) label = label+"(" + counter + "s)";
+    // }
+
+    IEnumerator RecoverCandy(){
+        counter = RecoverySeconds;
+        UpdateUI();
+        while(counter > 0){
+            yield return new WaitForSeconds(1.0f);
+            counter--;
+            UpdateUI();
+        }
+        AddCandy(1);
     }
 }
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    const int MaxShotPower = 5;
+    const int RecoverySeconds = 3;
+
+    int shotPower = MaxShotPower;
+
     public GameObject[] candyPrefabs;
     public Transform candyParentTransform;
     public CandyManager candyManager;
@@ -28,7 +33,8 @@ public class Shooter : MonoBehaviour
     }
 
     public void Shot(){
-        if(candyManager.GetCandyAmount()<=0)return;
+        if(candyManager.Candy<=0)return;
+        if(shotPower <= 0)return;
 
         GameObject candy = (GameObject)Instantiate(
             SampleCandy(),
@@ -42,5 +48,24 @@ public class Shooter : MonoBehaviour
         candyRigidBody.AddTorque(new Vector3(0,shotTorque,0));
 
         candyManager.ConsumeCandy();
+        ConsumePower();
+    }
+
+    void OnGUI()
+    {
+        GUI.color = Color.black;
+        string label = "";
+        for(int i = 0;i<shotPower;i++) label = label + "+";
+        GUI.Label(new Rect(50,65,100,30),label);    
+    }
+
+    void ConsumePower(){
+        shotPower--;
+        StartCoroutine(RecoveryPower());
+    }
+
+    IEnumerator RecoveryPower(){
+        yield return new WaitForSeconds(RecoverySeconds);
+        shotPower++;
     }
 }
